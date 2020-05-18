2962fe63c919e27cb88fb3b6f33fe1878f7fe018
mbhadange <65362340+mbhadange@users.noreply.github.com>
2020-05-18 18:18:38 -0400

    All the initial files needed.

---
+++ src/stale-code/search/Search.ts
@@ -0,0 +1,12 @@
+import {
+    ISODateString,
+    ISODateTimeString,
+    ISODateYearMonthString,
+    ISODateYearString,
+} from "polar-shared/src/metadata/ISODateTimeStrings";
+import {DOIStr} from "polar-shared/src/util/Strings";
+
+export namespace search {
+    let message: string='Hello World!';
+    console.log(message);
+}
