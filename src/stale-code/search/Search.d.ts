056b33b818e9ea5c9bd6886ed55d1a5558afaef6
mbhadange <65362340+mbhadange@users.noreply.github.com>
2020-05-20 11:53:27 -0400

    allows user to give input about directory

--- src/stale-code/search/Search.d.ts
+++ src/stale-code/search/Search.d.ts
@@ -1,2 +1,16 @@
-export declare namespace search {
+import { PathStr } from "polar-shared/src/util/Strings";
+export declare class Search {
+    static find(dir: string, opts?: Opts): ReadonlyArray<IFile>;
+}
+export declare type FileType = 'file' | 'directory';
+export declare type FileExt = string;
+export interface Opts {
+    readonly recurse?: boolean;
+    readonly types?: ReadonlyArray<FileType>;
+    readonly extensions?: ReadonlyArray<FileExt>;
+}
+export interface IFile {
+    readonly name: string;
+    readonly path: PathStr;
+    readonly type: FileType;
 }
