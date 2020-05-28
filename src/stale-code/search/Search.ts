92bce12fd350bed644230bcfb8375282c6e1c1af
mbhadange <65362340+mbhadange@users.noreply.github.com>
2020-05-28 12:41:13 -0400

    process.argv implemented

--- src/stale-code/search/Search.ts
+++ src/stale-code/search/Search.ts
@@ -94,92 +94,84 @@ export interface IFile {
 /// creates an empty map
 let hitMap = new Map();
 let opts = new DefaultOpts();
-/// allows us to take user input
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
 /**
- * asks user to see which directory to parse through
- * example: /Users/mihirmacpro13/Documents/GitHub/stale-finder-test-directory/
+ * uses process.argv to see which directory to parse through using the command line
+ * example: node Search.js /Users/mihirmacpro13/Documents/GitHub/stale-finder-test-directory/
  */ 
-rl.question('Enter the entire directory path to be parsed through: ', (currDirectory) => {
-    /// Search.find returns an array with all the files in the directory
-    /// iterates through each file in the directory
-    var fileMap = Search.find(currDirectory, opts);
-    for (var i = 0; i < fileMap.length; i++) {
-        /// map of the file type, name, path 
-        var file = fileMap[i];
-        var initialFileName = file.name;
-        var initialFilePath = file.path;
+var myArgs = process.argv[2];
+/// Search.find returns an array with all the files in the directory
+/// iterates through each file in the directory
+var fileMap = Search.find(myArgs, opts);
+for (var i = 0; i < fileMap.length; i++) {
+    /// map of the file type, name, path 
+    var file = fileMap[i];
+    var initialFileName = file.name;
+    var initialFilePath = file.path;
 
-        /// checks to see if the file name is test.ts
-        /// if it is then continues to the next file
-        if (file.name === 'test.ts') {
-            continue;
-        }
-        /// checks to make sure that the file type is either .ts or .tsx
-        else if (initialFileName.split('.').pop() === 'ts' || initialFileName.split('.').pop() === 'tsx') {
-            /// gets all the contents of the current file
-            const data = fs.readFileSync(initialFilePath,'utf8');
-            /// splits each line of data to allow us to parse through each one
-            const lines = data.split(/\r?\n/);
-            /// creates a regular expression for the import lines
-            let re = /import(?:["'\s]*([\w*{}\n\r\t, ]+)from\s*)?["'\s].*([@\w_-]+)["'\s].*;$/;
-            /// iterates through each line of the file
-            lines.forEach((line) => {
-                /// checks to see if the line matches the format of the regular expression
-                let importLine = line.match(re);
-                /// makes sure that the line actually has the proper format of the regEx
-                if (importLine != null) {
-                    /// gets the entire import lines contents
-                    let importVal = importLine[0];
-                    /// splits the line based off spaces and gets only the file path
-                    let filePath = importVal.split(' ').pop();
-                    var fullPath;
-                    /// converts that file path into a full file path
-                    if (filePath != undefined) {
-                        fullPath = path.resolve(initialFilePath, filePath);
-                    }
-                    /// checks to see if the hitmap already has that path
-                    if (hitMap.has(fullPath) === true) {
-                        /// if it does then increments the value of that file by 1
-                        var currVal = hitMap.get(fullPath);
-                        hitMap.set(fullPath, currVal + 1);
-                    }
-                    /// if the hitmap does not have that path as a key already
-                    else {
-                        /// then sets that file path to have a value of 1
-                        hitMap.set(fullPath, 1);
-                    }
+    /// checks to see if the file name is test.ts
+    /// if it is then continues to the next file
+    if (file.name === 'test.ts') {
+        continue;
+    }
+    /// checks to make sure that the file type is either .ts or .tsx
+    else if (initialFileName.split('.').pop() === 'ts' || initialFileName.split('.').pop() === 'tsx') {
+        /// gets all the contents of the current file
+        const data = fs.readFileSync(initialFilePath,'utf8');
+        /// splits each line of data to allow us to parse through each one
+        const lines = data.split(/\r?\n/);
+        /// creates a regular expression for the import lines
+        let re = /import(?:["'\s]*([\w*{}\n\r\t, ]+)from\s*)?["'\s].*([@\w_-]+)["'\s].*;$/;
+        /// iterates through each line of the file
+        lines.forEach((line) => {
+            /// checks to see if the line matches the format of the regular expression
+            let importLine = line.match(re);
+            /// makes sure that the line actually has the proper format of the regEx
+            if (importLine != null) {
+                /// gets the entire import lines contents
+                let importVal = importLine[0];
+                /// splits the line based off spaces and gets only the file path
+                let filePath = importVal.split(' ').pop();
+                var fullPath;
+                /// converts that file path into a full file path
+                if (filePath != undefined) {
+                    fullPath = path.resolve(initialFilePath, filePath);
                 }
-            });
-        }
+                /// checks to see if the hitmap already has that path
+                if (hitMap.has(fullPath) === true) {
+                    /// if it does then increments the value of that file by 1
+                    var currVal = hitMap.get(fullPath);
+                    hitMap.set(fullPath, currVal + 1);
+                }
+                /// if the hitmap does not have that path as a key already
+                else {
+                    /// then sets that file path to have a value of 1
+                    hitMap.set(fullPath, 1);
+                }
+            }
+        });
     }
+}
 
-    /**
-     * Sorts the map in order based on the values 
-     */
-    hitMap[Symbol.iterator] = function* () {
-        yield* [...this.entries()].sort((a, b) => a[1] - b[1]);
-    }
-    
-    /// makes the map into a table type format with a nested array
-    var updatedHitMap = [...hitMap];
+/**
+ * Sorts the map in order based on the values 
+ */
+hitMap[Symbol.iterator] = function* () {
+    yield* [...this.entries()].sort((a, b) => a[1] - b[1]);
+}
 
-    /**
-     * swaps the key and the value of the map, so the key is the number of hits and the value is the full file path
-     */
-    var finalHitMap = [];
-    for (var index = 0; index < updatedHitMap.length; index++) {
-        var currentArray = updatedHitMap[index];
-        var key = currentArray[0];
-        var value = currentArray[1];
-        finalHitMap[index] = [value, key];
-    }
-    /// prints out the hitMap
-    console.log(finalHitMap); 
-    rl.close();
-});
+/// makes the map into a table type format with a nested array
+var updatedHitMap = [...hitMap];
 
+/**
+ * swaps the key and the value of the map, so the key is the number of hits and the value is the full file path
+ */
+var finalHitMap = [];
+for (var index = 0; index < updatedHitMap.length; index++) {
+    var currentArray = updatedHitMap[index];
+    var key = currentArray[0];
+    var value = currentArray[1];
+    finalHitMap[index] = [value, key];
+}
+/// prints out the hitMap
+console.log(finalHitMap); 
