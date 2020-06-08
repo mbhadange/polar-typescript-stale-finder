a8027a4ba7877b9d9482a241b349fd9efc2c3ea3
mbhadange <65362340+mbhadange@users.noreply.github.com>
2020-06-08 12:18:48 -0400

    trying to get tests to work

--- src/stale-code/search/SearchTest.ts
+++ src/stale-code/search/SearchTest.ts
@@ -1,15 +1,8 @@
-/** 
-import {initializeTypescriptMapFiles, updateHitMap, checkFullPath, expandPath, 
-    sortMap, swapMapValues, printMap, getExtension, main} from './Search';
-import { expect } from 'chai';
- 
-describe ('CheckFullPath', () => {
-    it('should return full proper path after checking', () => {
-        var fileName = '/Users/mihirmacpro13/Documents/GitHub/polar-bookshelf/web/js/datastore/PersistenceLayer.ts'
-        var result = checkFullPath(fileName);
-        if (expect(result).to.equal('/Users/mihirmacpro13/Documents/GitHub/polar-bookshelf/web/js/datastore/PersistenceLayer.ts')) {
-            console.log("yes");
-        }
-    })
+import { getExtension } from "./Search";
+import { assert } from 'chai';
+
+describe ('GetExtension', function() {
+    it ("File extension getter", function() {
+        assert.equal(getExtension("file.txt"), "txt");
+    });
 });
-*/
